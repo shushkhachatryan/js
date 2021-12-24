7358518283f145f8291daac7d5a2d9305dbe6436
shushkhachatryan <xachatryan.207750.gmail.com>
2021-12-24 08:12:12 -0800

    initial commit

---
+++ main.js
@@ -0,0 +1,4 @@
+let str =
+  "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatem esse cum quae obcaecati minus soluta consequuntur impedit sunt architecto perspiciatis!";
+
+let text = str.slice(6, 11);
